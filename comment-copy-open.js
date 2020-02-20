758d6dc3a8b732cdf3cc1d30ffdb96a8a0ab80a2
Jag Mort <ilya@kortkeros.com>
2020-02-20 16:16:01 +0300

    Update comment-copy-open.js

--- comment-copy-open.js
+++ comment-copy-open.js
@@ -1,5 +1,3 @@
-//Browser bookmark URL: javascript:var comment="Комментарий";mainUrl='https://raw.githubusercontent.com/jagmort/automation/master/';function httpGet(theUrl){var xmlHttp=new XMLHttpRequest();xmlHttp.open("GET",theUrl,false);xmlHttp.send(null);return xmlHttp.responseText}if((document.location.href).indexOf('10.78.16.120:8080/argus/')>=0){actoinScript=mainUrl+'comment-copy-open.js';eval(httpGet(actoinScript))}
-
 if(comment.length > 0) {
     document.getElementById("group_def_tab-history_form-new_comment").value = comment;
     $( "button[id='group_def_tab-history_form-add_comment']" ).click();
