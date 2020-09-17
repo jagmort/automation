744a3c192fbd05c9f79c871d32a6f4f5ae67904d
Jag Mort <ilya@kortkeros.com>
2020-09-17 10:12:40 +0300

    Update comment-copy-open.js

--- comment-copy-open.js
+++ comment-copy-open.js
@@ -39,7 +39,7 @@ setTimeout(function() {
         var text = document.querySelector('.text');
         text.select();
         document.execCommand('copy');
-        window.open("http://omssis-sms.mts-nn.ru/sms2/");
+        if(typeof openSMS === 'undefined') window.open("http://omssis-sms.mts-nn.ru/sms2/");
     });
     document.getElementById('copyw').click();
 
